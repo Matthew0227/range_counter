afc308d856faeeda4c9723e4d7d039fcd976f9d7
Matthew0227 <matthewjaveniar2@gmail.com>
2024-10-27 00:47:30 +0800

    pseudo code

---
+++ range_counter.py
@@ -0,0 +1,3 @@
+#ranges list: bunch of variables for each specific range to store the numbers
+#function sorter: function will sort the number and push it to its designated list
+#core: while loop that asks the user to input a number until the user decided to stop
