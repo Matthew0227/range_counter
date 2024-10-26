89c72c35102ed024c2374e727682ffc877cfc7cb
Matthew0227 <matthewjaveniar2@gmail.com>
2024-10-27 01:45:11 +0800

    added an if statement to accept 1-50 value and print statement to tell the number was added to a range

--- range_counter.py
+++ range_counter.py
@@ -5,27 +5,40 @@ twenty1_to_thirty = []
 thirty1_to_forty = []
 forty1_to_fifty = []
 
+range_ = 0
 #function sorter: function will sort the number and push it to its designated list
 def range_sorter(input_placeholder):
     if 1 <= input_placeholder < 11:
         one_to_ten.append(input_placeholder)
+        range_ = "1-10"
+        return range_
     elif 11 <= input_placeholder < 21:
         eleven_to_twenty.append(input_placeholder)
+        range_ = "11-20"
+        return range_
     elif 21 <= input_placeholder < 31:
         twenty1_to_thirty.append(input_placeholder)
+        range_ = "21-30"
+        return range_
     elif 31 <= input_placeholder < 41:
         thirty1_to_forty.append(input_placeholder)
+        range_ = "31-40"
+        return range_
     elif 41 <= input_placeholder < 51:
         forty1_to_fifty.append(input_placeholder)
+        range_ = "41-50"
+        return range_
 #core: while loop that asks the user to input a number until the user decided to stop
 while True:
     while True:
         input_number = input("Please give a number between 1-50: ")
     
         if input_number.isdigit():
-            input_number = int(input_number)
-            range_sorter(input_number)
-            break
+            if 1 <= input_number < 51:
+                input_number = int(input_number)
+                range_sorter(input_number)
+                print("Your number was added to the range", range_)
+                break
         else:
             print("Please enter a number between 1-50")
     
